91dee36d4fda9ead1b2fc6ca63020c8592618115
Milagros Zea Cardenas <mily_zc@hotmail.com>
2019-10-25 04:23:56 -0300

    reportes

--- ReportesVentas/ReportesVentas/Principal.Designer.cs
+++ ReportesVentas/ReportesVentas/Principal.Designer.cs
@@ -33,6 +33,7 @@
             this.reportesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.bicicletasPorCategoriaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.marcasVendidasUltimoMesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -44,7 +45,7 @@
             this.reportesToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(844, 28);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -78,15 +79,26 @@
             this.marcasVendidasUltimoMesToolStripMenuItem.Text = "Marcas vendidas ultimo mes";
             this.marcasVendidasUltimoMesToolStripMenuItem.Click += new System.EventHandler(this.marcasVendidasUltimoMesToolStripMenuItem_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift Condensed", 68F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(799, 136);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Informes y reportes";
+            // 
             // Principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(844, 218);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Principal";
-            this.Text = "Form1";
+            this.Text = "Informes Reportes";
             this.Load += new System.EventHandler(this.Reportes_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
@@ -102,6 +114,7 @@
         private System.Windows.Forms.ToolStripMenuItem reportesToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem bicicletasPorCategoriaToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem marcasVendidasUltimoMesToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
     }
 }
 
